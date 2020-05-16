4e7d8bbb6f3cc9675549a41131625c663daa0bb4
Alexandr Deynenko <alex.deko.69@gmail.com>
2020-05-16 10:23:46 +0300

    add repostById in repositoryImpl

--- src/main/kotlin/com/post/repository/PostRepositoryImpl.kt
+++ src/main/kotlin/com/post/repository/PostRepositoryImpl.kt
@@ -4,6 +4,7 @@ import com.post.db.data.post.Posts
 import com.post.db.data.post.toPost
 import com.post.db.dbQuery
 import com.post.exception.ForbiddenException
+import com.post.exception.NotFoundException
 import com.post.model.PostModel
 import org.jetbrains.exposed.sql.*
 
@@ -87,16 +88,36 @@ class PostRepositoryImpl: PostRepository {
 
     override suspend fun dislikeById(id: Long): PostModel? {
         dbQuery {
-            Posts.update {
 
+            Posts.update {
             }
         }
         TODO("Not yet implemented")
     }
 
     override suspend fun repostById(id: Long): PostModel? {
+        val item = getById(id) ?: throw NotFoundException("Пост не найден")
         dbQuery {
-            Posts.select { Posts.id eq id }.copy()
+            Posts.insert { insertStatement ->
+                insertStatement[Posts.id] = item.id
+                insertStatement[Posts.ownerId] = item.ownerId
+                insertStatement[Posts.author] = item.author
+                insertStatement[Posts.createdDate] = item.createdDate
+                insertStatement[Posts.content] = item.content
+                insertStatement[Posts.countLike] = item.countLike
+                insertStatement[Posts.isLike] = item.isLike
+                insertStatement[Posts.countRepost] = item.countRepost
+                insertStatement[Posts.type] = item.type
+                insertStatement[Posts.adsUrl] = item.adsUrl
+                insertStatement[Posts.countViews] = item.countViews + 1L
+                insertStatement[Posts.parentId] = item.parentId
+                insertStatement[Posts.imageId] = item.imageId
+                insertStatement[Posts.videoUrl] = item.videoUrl
+                insertStatement[Posts.countComment] = item.countComment
+                insertStatement[Posts.isCanCommented] = item.isCanCommented
+                insertStatement[Posts.selectedLocation] = item.selectedLocation
+            }
+            Posts.select { Posts.id eq id }
         }
         TODO("Not yet implemented")
     }
