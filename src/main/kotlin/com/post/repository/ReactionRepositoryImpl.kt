2e25874f01eaf5d591ea70793384e9448347fb09
Alexandr Deynenko <alex.deko.69@gmail.com>
2020-06-08 16:07:13 +0300

    clean ReactionRepositoryImpl.kt

--- src/main/kotlin/com/post/repository/ReactionRepositoryImpl.kt
+++ src/main/kotlin/com/post/repository/ReactionRepositoryImpl.kt
@@ -1,20 +1,18 @@
 package com.post.repository
 
-import com.post.db.data.reaction.Reactions
+import com.post.db.data.reaction.Reaction
 import com.post.db.data.reaction.toReaction
 import com.post.db.dbQuery
 import com.post.model.ReactionModel
-import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.andWhere
 import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.selectAll
 
 class ReactionRepositoryImpl : ReactionRepository {
     override suspend fun geAllByPostId(id: Long): List<ReactionModel> =
         dbQuery {
-            Reactions.selectAll().andWhere {
-                Reactions.post_id eq id
+            Reaction.selectAll().andWhere {
+                Reaction.post_id eq id
             }.toList().map {
                 it.toReaction()
             }
@@ -23,7 +21,7 @@ class ReactionRepositoryImpl : ReactionRepository {
 
     override suspend fun save(item: ReactionModel) {
        dbQuery {
-           Reactions.insert { insertStatement ->
+           Reaction.insert { insertStatement ->
                insertStatement[post_id] = item.postId
                insertStatement[user_id] = item.userId
                insertStatement[created_date] = item.createdDate
