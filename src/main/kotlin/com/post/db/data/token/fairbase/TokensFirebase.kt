99a68cfc7fa2af658daef3548245aa16153a3ba1
Alexandr Deynenko <alex.deko.69@gmail.com>
2020-06-20 20:24:02 +0300

    TokensFirebase.kt named Table

--- src/main/kotlin/com/post/db/data/token/fairbase/TokensFirebase.kt
+++ src/main/kotlin/com/post/db/data/token/fairbase/TokensFirebase.kt
@@ -3,7 +3,7 @@ package com.post.db.data.token.fairbase
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Table
 
-object TokensFirebase : Table() {
+object TokensFirebase : Table("TOKEN_FCM") {
     val id: Column<Long> = long("id").autoIncrement().primaryKey()
     val user_id: Column<Long> = long("user_id")
     val token: Column<String> = varchar("token", 1000)
